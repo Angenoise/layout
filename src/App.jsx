3da9212fd294063efd3b99c31bdacbe556df8401
Angenoise <maf-delacruz.student@ua.edu.ph>
2025-09-04 14:09:24 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">    
+      <div className="header">Header</div>
+      <div className="main">      
+        <div className="left">
+          <div className="hero">Hero</div>
+          <div className="sidebar">Sidebar</div>
+        </div>
+        <div className="right">
+          <div className="main-content">Main Content</div>
+          <div className="extra">Extra Content</div>
+        </div>      
+      </div>
+      <div className="lower">
+        <div className="ri">Related Images</div>
+        <div className="rp">Related Posts</div>
+      </div>
+      <div className="footer">Footer</div>
+    </div>
+  )
+}
+
+export default App
